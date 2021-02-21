bf4a34d2f5d34550309ea368d90fb6c87e7e5196
Diego Borges <drborges.cic@gmail.com>
2021-02-21 15:33:49 -0300

    Simplify reconciliation logic

--- src/hooks/useSelectionReconciler.js
+++ src/hooks/useSelectionReconciler.js
@@ -1,12 +1,43 @@
-import { useEffect } from "react";
+import Context from "Context";
+import { useContext, useEffect, useMemo } from "react";
+import { useToggler } from "./useToggler";
 
-export function useSelectionReconciler(name, value, options, onChange) {
-  const option = options.find((o) => o.name === value?.name);
-  const reconcile = value && option && false;
+export function useSelectionReconciler(name, value, options) {
+  const { onChange } = useContext(Context);
+  const [expanded, toggleExpanded] = useToggler();
+
+  const disabled = options.length <= 1;
+  const optionById = useMemo(() => options.find((o) => o.id === value?.id), [options, value]);
+  const optionByName = useMemo(() => options.find((o) => o.name === value?.name), [options, value]);
+  const canAutoSelect = !optionById && options.length === 1;
+  const canReconcile = !optionById && optionByName;
+  const cantReconcile = !optionById && !optionByName;
+  const invalid = cantReconcile && !disabled;
 
   useEffect(() => {
-    if (reconcile) onChange(name, option);
-  }, [onChange, name, option, reconcile]);
+    if (canAutoSelect) {
+      onChange(name, options[0]);
+    } else if (canReconcile) {
+      onChange(name, optionByName);
+    } else if (invalid && !expanded) {
+      toggleExpanded();
+    }
+  }, [
+    invalid,
+    onChange,
+    name,
+    expanded,
+    canAutoSelect,
+    canReconcile,
+    optionByName,
+    options,
+    toggleExpanded
+  ]);
 
-  return reconcile;
+  return {
+    disabled,
+    expanded,
+    invalid,
+    toggleExpanded
+  };
 }
